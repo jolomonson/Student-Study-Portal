edadeeb3efb38ef27ea7bd4c006bc9af7be33270
Mary Solomon <jolomonson@gmail.com>
2022-05-07 08:09:51 +0100

    Created and registered NotesAdmin
    Signed-off-by: Mary Solomon <jolomonson@gmail.com>

--- studentstudyportal/dashboard/admin.py
+++ studentstudyportal/dashboard/admin.py
@@ -1,4 +1,16 @@
 from django.contrib import admin
 from . models import *
 # Register your models here.
-admin.site.register(Notes)
+
+class NotesAdmin(admin.ModelAdmin):
+    list_display = (
+        'user',
+        'title',
+        'description'
+    )
+    list_filter = (
+        'title',
+        'description'
+    )
+
+admin.site.register(Notes, NotesAdmin)
