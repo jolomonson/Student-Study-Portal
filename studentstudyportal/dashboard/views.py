c74c8e6f7463663be1aaff92518a1f16eded0837
Mary Solomon <jolomonson@gmail.com>
2022-05-07 08:32:25 +0100

    Created NotesForm()
    Signed-off-by: Mary Solomon <jolomonson@gmail.com>

--- studentstudyportal/dashboard/views.py
+++ studentstudyportal/dashboard/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
-from .models import Notes
+from .forms import *
 
 # Create your views here.
 def home(request):
     return render(request, 'dashboard/home.html')
 
 def notes(request):
+    form = NotesForm()
     notes = Notes.objects.filter(user=request.user)
     data = {'notes':notes}
     return render(request, 'dashboard/notes.html', data)
