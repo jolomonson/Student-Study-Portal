002c3e3d2ef60485b0ed6f1b7bb3a4b19cd7f743
Mary Solomon <jolomonson@gmail.com>
2022-05-07 09:53:53 +0100

    Implemented CRUD functionality for Creating notes
    Signed-off-by: Mary Solomon <jolomonson@gmail.com>

--- studentstudyportal/dashboard/views.py
+++ studentstudyportal/dashboard/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
 from .forms import *
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
     return render(request, 'dashboard/home.html')
 
 def notes(request):
-    form = NotesForm()
+    if request.method == "POST":
+        form = NotesForm(request.POST)
+        if form.is_valid():
+            notes = Notes(user=request.user, title=request.POST['title'],description=request.POST['description'])
+            notes.save()
+        messages.success(request, 'The notes "{}" was added successfully'.format(notes.title))
+    else:
+        form = NotesForm()
     notes = Notes.objects.filter(user=request.user)
     data = {'notes':notes, 'form':form}
     return render(request, 'dashboard/notes.html', data)
