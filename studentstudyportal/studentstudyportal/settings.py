9d4c990dccb2094764c7965e9e0cb88f943f46b8
preciousimo <preciousimoniakemu@gmail.com>
2022-05-11 15:40:20 +0100

    Fix whiteNoise module not found

--- studentstudyportal/studentstudyportal/settings.py
+++ studentstudyportal/studentstudyportal/settings.py
@@ -48,8 +48,8 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     #Heroku Deployment
     #Comment Line 51
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -145,8 +145,8 @@ CRISPY_TEMPLATE_PACK = "bootstrap4"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
-STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_ROOT  =   os.pathjoin(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/
 
 # Extra lookup directories for collectstatic to find static files
 STATICFILES_DIRS = (
@@ -154,7 +154,7 @@ STATICFILES_DIRS = (
 )
 
 #  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 import dj_database_url 
 prod_db  =  dj_database_url.config(conn_max_age=500)
